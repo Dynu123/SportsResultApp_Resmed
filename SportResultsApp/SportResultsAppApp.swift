c38bb19ed38c3a2a1527f5c8bc7b7a124e095b3a
Dyana Varghese <dyna1.arackal@gmail.com>
2022-08-21 19:24:18 +0100

    Initial Commit

---
+++ SportResultsApp/SportResultsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SportResultsAppApp.swift
+//  SportResultsApp
+//
+//  Created by Dyana Varghese on 21/08/22.
+//
+
+import SwiftUI
+
+@main
+struct SportResultsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
